f886881e94ac54b9a7614829619af8aa1c8aa5d0
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-04-03 13:37:40 +0100

    Add sample mutation

--- server/src/App.vue
+++ server/src/App.vue
@@ -1,24 +1,90 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div>
+      <amplify-connect
+        :subscription="onCreatePatientSubscription"
+        :mutation="createPatientMutation"
+        @done="onCreatePatient"
+      >
+        <template slot-scope="{ loading, mutate, errors }">
+          <div v-if="loading">Loading...</div>
+
+          <div v-else-if="errors.length > 0">
+            <ul v-for="item in errors">
+              <li>{{item.message}}</li>
+            </ul>
+          </div>
+
+          <input v-model="id" placeholder="Patient ID Number">
+          <input v-model="phone" placeholder="Phone Number">
+          <input v-model="countryCode" placeholder="County Code">
+          <button v-on:click="mutate">Create Patient Profile</button>
+        </template>
+      </amplify-connect>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { components } from "aws-amplify-vue";
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { listPatients } from "./graphql/queries.js";
+import { createPatient } from "./graphql/mutations";
+import { onCreatePatient } from "./graphql/subscriptions";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ...components
+  },
+  data() {
+    return {
+      id: null,
+      phone: null,
+      countryCode: null
+    };
+  },
+  computed: {
+    listPatientsQuery() {
+      return this.$Amplify.graphqlOperation(listPatients);
+    },
+    createPatientMutation() {
+      const input = {
+        phone: this.phone,
+        countryCode: this.countryCode
+      };
+      return this.$Amplify.graphqlOperation(createPatient, { input });
+    },
+    onCreatePatientSubscription() {
+      return this.$Amplify.graphqlOperation(onCreatePatient);
+    }
+  },
+  methods: {
+    createPatient() {
+      const input = {
+        phone: this.phone,
+        countryCode: this.countryCode
+      };
+      try {
+        API.graphql(graphqlOperation(createPatient, { input }));
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    onCreatePatient(prevData, newData) {
+      const newPatient = newData.onCreatePatient;
+      prevDta.data.listPatients.items.push(newPatient);
+      return prevData.data;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- server/src/main.js
+++ server/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+import aws_exports from "./aws-exports";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Amplify.configure(aws_exports);
+
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
